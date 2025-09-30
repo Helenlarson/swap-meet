78de2f81dcdd6d3e1cf49fce34c0f2522e1df205
Helen <lelinewborncare@gmail.com>
2025-09-30 14:59:32 -0700

    Working in progress for wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,32 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0):
         if id is None:
             self.id = uuid.uuid4().int
         else: 
             self.id = id
 
+        self.condition = condition
+
     def get_category(self):
         return "Item"
     
+    def condition_description(self):
+        descriptions = {
+            0: "Worst condition",
+            1: "Heavily used",
+            2: "Fair",
+            3: "Good",
+            4: "Like new",
+            5: "Best condition"
+        }
+
+        if self.condition in descriptions:
+            return descriptions[self.condition]
+        else:
+            return False
+
     def __str__(self):
         return f"An object of type Item with id {self.id}."
 
